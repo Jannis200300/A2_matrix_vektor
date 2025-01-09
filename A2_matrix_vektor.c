2f71aa7446b34c80211b57683880fbffa2ddda92
jannis2320 <jh963108@fh-muenster.de>
2025-01-09 12:01:42 +0100

    123

--- A2_matrix_vektor.c
+++ A2_matrix_vektor.c
@@ -9,36 +9,54 @@ int matrix_a[ZEILEN][SPALTEN];
 int vektor_a[ZEILEN];
 int vektor_b[ZEILEN];
 
-int main(void) {
-    int c[4];
-    int i = 0, j = 0;
+/* Funktion zur Initialisierung von Matrix und Vektor */
+void initialize_matrix_and_vector() {
+    int i, j;
+    for (i = 0; i < ZEILEN; i++) {
+        for (j = 0; j < SPALTEN; j++) {
+            matrix_a[i][j] = (rand() % 64) - 32; /* Werte von -32 bis 31 */
+        }
+        vektor_a[i] = (rand() % 64) - 32; /* Werte von -32 bis 31 */
+    }
+}
 
-    srand((unsigned int)time(NULL)); /* vorgegeben */
-    printf("Kontrolle:");
-    for(i=0; i<4; i++){
-        for(j=0; j<4; j++){
-            matrix_a[i][j] = (rand() % 64) - 32; /* vorgegeben */
-            printf("%4d",matrix_a[i][j]);
+/* Funktion zur Matrix-Vektor-Multiplikation */
+void matrix_vector_multiplication() {
+    int i, j;
+    for (i = 0; i < ZEILEN; i++) {
+        vektor_b[i] = 0;
+        for (j = 0; j < SPALTEN; j++) {
+            vektor_b[i] += matrix_a[i][j] * vektor_a[j];
         }
     }
-    for(i=0; i<4; i++) {
-        vektor_b[i]= (rand() % 64) - 32;
-    }
-    printf("\n Matrix A:\n");
-    for (i = 0; i < 4; i++) {
-        for (j = 0; j < 4; j++) {
-            c[i] += matrix_a[i][j] * vektor_b[j];
-            printf("%4d",matrix_a[i][j]);
+}
+
+/* Funktion zur Ausgabe der Matrix und Vektoren */
+void print_matrix_and_vectors() {
+    int i, j;
+    printf("Matrix A:\n");
+    for (i = 0; i < ZEILEN; i++) {
+        for (j = 0; j < SPALTEN; j++) {
+            printf("%4d ", matrix_a[i][j]);
         }
         printf("\n");
     }
-    printf("\nVektor b:\n");
-    for (j = 0; j < 4; j++) {
-        printf("%4d\n", vektor_b[j]);
+    printf("\nVektor A:\n");
+    for (i = 0; i < ZEILEN; i++) {
+        printf("%4d\n", vektor_a[i]);
     }
-    printf("\nErgebnisvektor c:\n");
-    for (i = 0; i < 4; i++) {
-        printf("%4d\n", c[i]);
+    printf("\nErgebnisvektor B:\n");
+    for (i = 0; i < ZEILEN; i++) {
+        printf("%4d\n", vektor_b[i]);
     }
+}
+
+int main() {
+
+    srand((unsigned int)time(NULL));
+    initialize_matrix_and_vector();
+    matrix_vector_multiplication();
+    print_matrix_and_vectors();
+
     return 0;
 }
