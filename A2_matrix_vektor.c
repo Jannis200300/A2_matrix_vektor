2089adf1ebef38887903ef0b05b56656946b58e3
jannis2320 <jh963108@fh-muenster.de>
2025-01-09 14:59:33 +0100

    123

--- A2_matrix_vektor.c
+++ A2_matrix_vektor.c
@@ -5,15 +5,15 @@
 #define ZEILEN 4
 #define SPALTEN 4
 
-int matrix_a[ZEILEN][SPALTEN];
-int vektor_a[ZEILEN];
-int vektor_b[ZEILEN];
+int matrix_a[200][200];
+int vektor_a[200];
+int vektor_b[200];
 
 /* Funktion zur Initialisierung von Matrix und Vektor */
-void initialize_matrix_and_vector() {
+void initialize_matrix_and_vector(int const a, int const b) {
     int i, j;
-    for (i = 0; i < ZEILEN; i++) {
-        for (j = 0; j < SPALTEN; j++) {
+    for (i = 0; i < a; i++) {
+        for (j = 0; j < b; j++) {
             matrix_a[i][j] = (rand() % 64) - 32; /* Werte von -32 bis 31 */
         }
         vektor_a[i] = (rand() % 64) - 32; /* Werte von -32 bis 31 */
@@ -21,42 +21,56 @@ void initialize_matrix_and_vector() {
 }
 
 /* Funktion zur Matrix-Vektor-Multiplikation */
-void matrix_vector_multiplication() {
+void matrix_vector_multiplication(int const a, int const b) {
     int i, j;
-    for (i = 0; i < ZEILEN; i++) {
+    for (i = 0; i < a; i++) {
         vektor_b[i] = 0;
-        for (j = 0; j < SPALTEN; j++) {
+        for (j = 0; j < b; j++) {
             vektor_b[i] += matrix_a[i][j] * vektor_a[j];
         }
     }
 }
 
 /* Funktion zur Ausgabe der Matrix und Vektoren */
-void print_matrix_and_vectors() {
+void print_matrix_and_vectors(int const a, int const b) {
     int i, j;
     printf("Matrix A:\n");
-    for (i = 0; i < ZEILEN; i++) {
-        for (j = 0; j < SPALTEN; j++) {
+    for (i = 0; i < a; i++) {
+        for (j = 0; j < b; j++) {
             printf("%4d ", matrix_a[i][j]);
         }
         printf("\n");
     }
     printf("\nVektor A:\n");
-    for (i = 0; i < ZEILEN; i++) {
+    for (i = 0; i < a; i++) {
         printf("%4d\n", vektor_a[i]);
     }
     printf("\nErgebnisvektor B:\n");
-    for (i = 0; i < ZEILEN; i++) {
+    for (i = 0; i < a; i++) {
         printf("%4d\n", vektor_b[i]);
     }
 }
 
 int main() {
+    double timeEnd, timeBegin;
+    int i;
+    for(i=-2;i<=197;i++) {
 
-    srand((unsigned int)time(NULL));
-    initialize_matrix_and_vector();
-    matrix_vector_multiplication();
-    print_matrix_and_vectors();
-
+        timeBegin = clock();
+        srand((unsigned int)time(NULL));
+        initialize_matrix_and_vector(ZEILEN+i,SPALTEN+i);
+        matrix_vector_multiplication(ZEILEN+i,SPALTEN+i);
+        /*
+        print_matrix_and_vectors(ZEILEN+i,SPALTEN+i);
+        */
+        printf("\n Dimension %d x %d \n",i+3,i+3);
+        timeEnd= clock();
+        printf("\nClock ticks started at: %f\n", timeBegin);
+        printf("Clock ticks ended at: %f\n", timeEnd);
+        timeEnd = timeEnd - timeBegin;
+        printf("Clock ticks needed: %f\n", timeEnd);
+        timeEnd = timeEnd / CLOCKS_PER_SEC;
+        printf("Time needed in seconds: %f\n", timeEnd);
+    }
     return 0;
 }
